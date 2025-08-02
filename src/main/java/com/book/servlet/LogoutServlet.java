2a551939a6cf28c384c3a71d62e583af308c2f5d
vidurangiwijerathna <vidurangiwijerathna@gmail.com>
2025-08-02 09:10:35 +0530

    init commit

--- src/main/java/com/book/servlet/LogoutServlet.java
+++ src/main/java/com/book/servlet/LogoutServlet.java
@@ -1,37 +1,20 @@
 package com.book.servlet;
 
-import com.book.dao.UserDAO;
-import com.book.entity.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
 import java.io.IOException;
 
-@WebServlet("/register")
-public class RegisterServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String name = request.getParameter("name");
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String role = request.getParameter("role"); // ADMIN or CASHIER
-
-        try {
-            User user = new User();
-            user.setName(name);
-            user.setEmail(email);
-            user.setPassword(password);
-            user.setRole(role);
-
-            UserDAO userDAO = new UserDAO();
-            userDAO.save(user);
-
-            response.sendRedirect("login.jsp");
-
-        } catch (Exception e) {
-            throw new ServletException(e);
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
         }
+        response.sendRedirect("index.jsp");
     }
 }
